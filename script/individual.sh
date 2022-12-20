0083426db98dc840296528e284aab19245135274
unknown <hej@aspoblourib.1w1.one>
2022-12-20 20:47:05 +0700

    individual.sh : another make sure to create folder

--- script/individual.sh
+++ script/individual.sh
@@ -5,17 +5,23 @@ mkdir -p ./content
 
 mkdir -p ./content/DNSBlocklist
 
-wget -O ./content/DNSBlocklist/domain/allowlist.txt https://github.com/minoplhy/DNSBlocklist/raw/main/domain/allowlist.txt
-wget -O ./content/DNSBlocklist/domain/denylist.txt https://github.com/minoplhy/DNSBlocklist/raw/main/domain/denylist.txt
-wget -O ./content/DNSBlocklist/rpz/denylist.rpz https://github.com/minoplhy/DNSBlocklist/raw/main/rpz/denylist.rpz
-wget -O ./content/DNSBlocklist/rpz/allowlist.rpz https://github.com/minoplhy/DNSBlocklist/raw/main/rpz/allowlist.rpz
-wget -O ./content/DNSBlocklist/rpz/cname-cloaking.rpz https://github.com/minoplhy/DNSBlocklist/raw/main/rpz/cname-cloaking.rpz
-wget -O ./content/DNSBlocklist/adblock/allowlist.txt https://github.com/minoplhy/DNSBlocklist/raw/main/adblock/allowlist.txt
-wget -O ./content/DNSBlocklist/adblock/denylist.txt https://github.com/minoplhy/DNSBlocklist/raw/main/adblock/denylist.txt
+mkdir -p ./content/DNSBlocklist/domain
+wget -O  ./content/DNSBlocklist/domain/allowlist.txt https://github.com/minoplhy/DNSBlocklist/raw/main/domain/allowlist.txt
+wget -O  ./content/DNSBlocklist/domain/denylist.txt https://github.com/minoplhy/DNSBlocklist/raw/main/domain/denylist.txt
+
+mkdir -p ./content/DNSBlocklist/rpz
+wget -O  ./content/DNSBlocklist/rpz/denylist.rpz https://github.com/minoplhy/DNSBlocklist/raw/main/rpz/denylist.rpz
+wget -O  ./content/DNSBlocklist/rpz/allowlist.rpz https://github.com/minoplhy/DNSBlocklist/raw/main/rpz/allowlist.rpz
+wget -O  ./content/DNSBlocklist/rpz/cname-cloaking.rpz https://github.com/minoplhy/DNSBlocklist/raw/main/rpz/cname-cloaking.rpz
+
+mkdir -p ./content/DNSBlocklist/adblock
+wget -O  ./content/DNSBlocklist/adblock/allowlist.txt https://github.com/minoplhy/DNSBlocklist/raw/main/adblock/allowlist.txt
+wget -O  ./content/DNSBlocklist/adblock/denylist.txt https://github.com/minoplhy/DNSBlocklist/raw/main/adblock/denylist.txt
 
 # uncategories
 
 mkdir -p ./content/uncategories
 
-wget -O ./content/uncategories/filters/blocked.txt https://github.com/minoplhy/filters/raw/main/Resources/blocked.txt
-wget -O ./content/uncategories/filters/excluded.txt https://github.com/minoplhy/filters/raw/main/Resources/excluded.txt
+mkdir -p ./content/uncategories/filters
+wget -O  ./content/uncategories/filters/blocked.txt https://github.com/minoplhy/filters/raw/main/Resources/blocked.txt
+wget -O  ./content/uncategories/filters/excluded.txt https://github.com/minoplhy/filters/raw/main/Resources/excluded.txt
